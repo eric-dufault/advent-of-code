0301291ad124ced2f0da54e9ec26049fe83b18a3
Eric Dufault <ericjcdufault@gmail.com>
2022-12-06 11:49:19 -0600

    Day 6

--- src/org/personal/adventofcode/y2022/day06/Main.java
+++ src/org/personal/adventofcode/y2022/day06/Main.java
@@ -12,26 +12,25 @@ public class Main {
     List<String> lines = Files.readAllLines(Paths.get("C:\\Users\\Eric\\Projects\\advent-of-code\\src\\org\\personal\\adventofcode\\y2022\\day06\\input06.txt"));
 
     Packet packet = getPacket(lines.get(0), 4);
-    System.out.println(packet.getPackendEndIndex() + " " + packet.getPacketString());
+    System.out.print(packet.getPackendEndIndex() + " " + packet.getPacketString());
     System.out.println();
 
     packet = getPacket(lines.get(0), 14);
-    System.out.println(packet.getPackendEndIndex() + " " + packet.getPacketString());
+    System.out.print(packet.getPackendEndIndex() + " " + packet.getPacketString());
     System.out.println();
   }
 
   private static Packet getPacket(String datastream, int packetSize) {
     Packet packet = null;
 
-    for(int i = 0; i < datastream.length(); i++) {
-      Set<Character> testPacketItems = new HashSet<>();
-      int j = i;
-      while(j < datastream.length() && j < i + packetSize) {
-        testPacketItems.add(datastream.charAt(j));
-        j++;
-      }
-      if (testPacketItems.size() == packetSize) {
-        packet = new Packet(j, datastream.substring(i, j));
+    for (int i = packetSize; i < datastream.length(); i++) {
+      String candidate = datastream.substring(i-packetSize, i);
+      Set<Character> testUniqueness = new HashSet<>();
+      for (int j = 0; j < candidate.length(); j++)
+        testUniqueness.add(candidate.charAt(j));
+
+      if (testUniqueness.size() == candidate.length()) {
+        packet = new Packet(i, candidate);
         break;
       }
     }
